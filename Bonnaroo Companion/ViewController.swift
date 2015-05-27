b553303d1058799ca07883e7643071056536d2aa
Michael Durante <mikedurante@Mikes-MacBook-Pro.local>
2015-05-27 17:00:30 -0500

    Init commit

---
+++ Bonnaroo Companion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bonnaroo Companion
+//
+//  Created by Mike Durante on 5/27/15.
+//  Copyright (c) 2015 Michael Durante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
